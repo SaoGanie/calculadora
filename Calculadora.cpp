7f63a14e3887b1d5ddb0848bcbef7a6b5fcb28f1
Henrique Alves <91351316+SaoGanie@users.noreply.github.com>
2023-03-15 10:46:11 -0300

    Add files via upload

---
+++ Calculadora.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+float calcula(float valor_inicial, float margem_lucro);
+
+int main() {
+    char resposta;
+    float valor_inicial, margem_lucro, valor_final;
+
+    do {
+        printf("Informe o valor inicial do produto: ");
+        scanf("%f", &valor_inicial);
+        printf("Informe a margem de lucro desejada (em porcentagem): ");
+        scanf("%f", &margem_lucro);
+
+        // cálculo do valor de venda
+        valor_final = calcula(valor_inicial, margem_lucro);
+
+        // exibição do resultado
+        printf("O valor de venda do produto com margem de lucro de %.2f%% e acréscimo de 5%% é R$ %.2f \n", margem_lucro, valor_final);
+
+        printf("Deseja continuar? s/n\n");
+        scanf(" %c", &resposta);
+    } while (resposta == 's');
+
+    return 0;
+}
+
+float calcula(float valor_inicial, float margem_lucro) {
+    float valor_venda = valor_inicial * (1 + (margem_lucro / 100));
+    float valor_final = valor_venda / 0.95;
+    return valor_final;
+}
+
